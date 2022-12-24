69705f7cbf11d54af62ec8f5e97e72513119b286
Kycb42148 <kycb42148@gmail.com>
2022-12-24 16:49:44 +0200

    Initial commit

---
+++ src/inside/Template.java
@@ -0,0 +1,19 @@
+package inside;
+
+import arc.util.*;
+import mindustry.mod.*;
+
+public class Template extends Plugin {
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+    }
+}
